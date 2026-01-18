4b5eb604005df20b9e32612d8962aa0a3a177a12
musishere <mustafabukhari333@gmail.com>
2026-01-18 20:01:08 +0500

    Health Route and Handlers structure

--- internal/api/server.go
+++ internal/api/server.go
@@ -1,12 +1,18 @@
 package api
 
 import (
+	"net/http"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/musishere/Mustafa-Ecommerce-App/configs"
 )
 
 func StartServer(appconfig configs.Appconfig) {
 	app := fiber.New()
-
+	app.Get("/health", HealthCheck)
 	app.Listen(appconfig.ServerPort)
+}
+
+func HealthCheck(ctx *fiber.Ctx) error {
+	return ctx.Status(http.StatusOK).JSON(&fiber.Map{"message": "Healthy!"})
 }
