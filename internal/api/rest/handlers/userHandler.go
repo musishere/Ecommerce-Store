bd9f24764e68b352a035b43d6905281944fb6b99
musishere <mustafabukhari333@gmail.com>
2026-01-18 20:18:10 +0500

    added handlers

--- internal/api/rest/handlers/userHandler.go
+++ internal/api/rest/handlers/userHandler.go
@@ -1,1 +1,69 @@
 package handlers
+
+import (
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/musishere/Mustafa-Ecommerce-App/internal/api/rest"
+)
+
+type UserHandler struct {
+	//	svc UserService
+}
+
+func SetUpUserRoutes(restHandler *rest.RestHandler) {
+	app := restHandler.App
+
+	handler := UserHandler{}
+	//	public endpoints
+	app.Post("/register", handler.Register)
+	app.Post("/login", handler.Login)
+
+	//private endpoints
+	app.Get("/verify", handler.GetVerificationCode)
+	app.Post("/verify", handler.Verify)
+	app.Post("/profile", handler.CreateProfile)
+	app.Get("/profile", handler.GetProfile)
+	app.Post("/cart", handler.AddToCart)
+	app.Get("/cart", handler.GetCart)
+	app.Get("/order", handler.Login)
+	app.Get("/order/:id", handler.Login)
+	app.Post("/become-seller", handler.Login)
+
+}
+
+func (handler *UserHandler) Register(ctx *fiber.Ctx) error {
+	return ctx.Status(http.StatusOK).JSON(&fiber.Map{"message": "User Registered!"})
+}
+func (handler *UserHandler) Login(ctx *fiber.Ctx) error {
+	return ctx.Status(http.StatusOK).JSON(&fiber.Map{"message": "User Login!"})
+}
+func (handler *UserHandler) Verify(ctx *fiber.Ctx) error {
+	return ctx.Status(http.StatusOK).JSON(&fiber.Map{"message": "Verify!"})
+}
+func (handler *UserHandler) GetVerificationCode(ctx *fiber.Ctx) error {
+	return ctx.Status(http.StatusOK).JSON(&fiber.Map{"message": "Verification Code!"})
+}
+func (handler *UserHandler) GetProfile(ctx *fiber.Ctx) error {
+	return ctx.Status(http.StatusOK).JSON(&fiber.Map{"message": "get profile!"})
+}
+func (handler *UserHandler) CreateProfile(ctx *fiber.Ctx) error {
+	return ctx.Status(http.StatusOK).JSON(&fiber.Map{"message": "create profile!"})
+}
+func (handler *UserHandler) AddToCart(ctx *fiber.Ctx) error {
+	return ctx.Status(http.StatusOK).JSON(&fiber.Map{"message": "add to cart!"})
+}
+func (handler *UserHandler) GetCart(ctx *fiber.Ctx) error {
+	return ctx.Status(http.StatusOK).JSON(&fiber.Map{"message": "get cart!"})
+}
+func (handler *UserHandler) CreateOrder(ctx *fiber.Ctx) error {
+	return ctx.Status(http.StatusOK).JSON(&fiber.Map{"message": "Create order!"})
+}
+func (handler *UserHandler) GetOrders(ctx *fiber.Ctx) error {
+
+	return ctx.Status(http.StatusOK).JSON(&fiber.Map{"message": "GetOrders!"})
+}
+func (handler *UserHandler) GetOrder(ctx *fiber.Ctx) error {
+
+	return ctx.Status(http.StatusOK).JSON(&fiber.Map{"message": "Getorder!"})
+}
